dc1dbe007b008101553f839d39985df0c144b8a5
Joaolluiz <joao.fdk8@gmail.com>
2023-07-31 15:54:43 -0300

    Service layer, component registration

--- src/main/java/com/webservice/webserviceSpring/resources/UserResource.java
+++ src/main/java/com/webservice/webserviceSpring/resources/UserResource.java
@@ -1,20 +1,32 @@
 package com.webservice.webserviceSpring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webservice.webserviceSpring.entities.User;
+import com.webservice.webserviceSpring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	// ResponseEntity -- Tipo que responde a requisições Web
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Marcela", "marcela@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public User findById(@PathVariable Long id) {
+		return userService.findById(id);
 	}
 }
--- src/main/java/com/webservice/webserviceSpring/services/UserService.java
+++ src/main/java/com/webservice/webserviceSpring/services/UserService.java
@@ -1,5 +1,24 @@
 package com.webservice.webserviceSpring.services;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webservice.webserviceSpring.entities.User;
+import com.webservice.webserviceSpring.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		return userRepository.findById(id).get();
+	}
 }
