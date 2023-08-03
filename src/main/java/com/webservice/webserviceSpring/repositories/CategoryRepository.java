d48b7fec29547c6e16c41233826c0dcb99df1b18
Joaolluiz <joao.fdk8@gmail.com>
2023-08-02 23:15:01 -0300

    Entity Category

---
+++ src/main/java/com/webservice/webserviceSpring/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.webservice.webserviceSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservice.webserviceSpring.entities.Category;
+
+public interface CategoryRepository extends JpaRepository <Category, Long> {
+
+}
