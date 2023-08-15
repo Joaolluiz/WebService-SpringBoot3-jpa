430b6cd896336ec2851138e8448e8ac841e15f2b
Joaolluiz <joao.fdk8@gmail.com>
2023-08-14 21:55:12 -0300

    User insert

--- src/main/java/com/webservice/webserviceSpring/resources/UserResource.java
+++ src/main/java/com/webservice/webserviceSpring/resources/UserResource.java
@@ -1,13 +1,18 @@
 package com.webservice.webserviceSpring.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webservice.webserviceSpring.entities.User;
 import com.webservice.webserviceSpring.services.UserService;
@@ -29,4 +34,22 @@ public class UserResource {
 	public User findById(@PathVariable Long id) {
 		return userService.findById(id);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		User newUser = userService.insert(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+	}
+	
+	/* EndPoint alternativo para inserir e aparecer o código 201.
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	*/
+	
 }
