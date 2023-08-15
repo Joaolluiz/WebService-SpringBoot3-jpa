13fbd0ffd2c82f07e48648add0f36f23e4005e22
Joaolluiz <joao.fdk8@gmail.com>
2023-08-14 22:25:34 -0300

    User Update

--- src/main/java/com/webservice/webserviceSpring/resources/UserResource.java
+++ src/main/java/com/webservice/webserviceSpring/resources/UserResource.java
@@ -1,20 +1,18 @@
 package com.webservice.webserviceSpring.resources;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webservice.webserviceSpring.entities.User;
 import com.webservice.webserviceSpring.services.UserService;
@@ -60,4 +58,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+		User updateUser = userService.update(id, user);
+		return ResponseEntity.ok().body(updateUser);		
+	}
 }
